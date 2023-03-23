fc387b36ca18c99e9948b23fc44f68e1d0589af7
Matthew Todor <unclejunglegeorge@gmail.com>
2023-03-22 21:47:34 -0600

    added more packages, set up routing, added first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Home from "./pages/Home/home";
+const theme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
 function App() {
-	return <div className="App"></div>;
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { Typography } from "@mui/material";
+
+const Home = (): JSX.Element => {
+	return (
+		<Grid container>
+			<Typography>Hi Mom</Typography>
+		</Grid>
+	);
+};
+
+export default Home;
